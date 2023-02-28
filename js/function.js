744d1791a77d0a60fb3e85ed428f43ff2e88f783
Franco Pautasso <frpautasso@gmail.com>
2023-02-28 19:47:46 -0300

    Proyecto Final

---
+++ js/function.js
@@ -0,0 +1,27 @@
+
+
+document.getElementById('boton').addEventListener('click', function(){
+    
+    document.getElementById("demo").innerHTML = "Muchas Gracias"
+});
+
+
+document.getElementById('botonJs').addEventListener('click', function(){
+    
+    document.getElementById("demo2").innerHTML = "Desarrollando curso de JavaScript en la plataforma ticmas"
+});
+
+document.getElementById('botonDw').addEventListener('click', function(){
+    
+    document.getElementById("demo3").innerHTML = "Desarrollando curso de Desarrollo Web/Css en la plataforma ticmas"
+});
+
+document.getElementById('botonIm').addEventListener('click', function(){
+    
+    document.getElementById("demo4").innerHTML = "Facultad de ciencias exactas Fisicas y Naturales"
+});
+
+document.getElementById('botonS').addEventListener('click', function(){
+    
+    document.getElementById("demo5").innerHTML = "Orientacion en economia y gestion de las organizaciones"
+});
